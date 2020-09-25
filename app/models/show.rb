c0e747277734f0eef73092e59ae0d7787062699c
Victorsloop <torres.victor136@gmail.com>
2020-09-25 19:21:14 -0400

    Completed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
+
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        self.characters.map{|characters| characters.actor.full_name}
+    end
+
   
 end
